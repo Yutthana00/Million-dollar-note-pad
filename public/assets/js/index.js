7960a22265441ae46e86798f4408542257b5dad9
Yutthana00 <arf101293@gmail.com>
2022-05-19 23:27:29 +0100

    Returns HTML element with/without a delete button

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -154,6 +154,21 @@ const renderNoteList = async (notes) => {
     return liEl;
   };
 
+  if (jsonNotes.length === 0) {
+    noteListItems.push(createLi('No saved Notes', false));
+  }
+
+  jsonNotes.forEach((note) => {
+    const li = createLi(note.title);
+    li.dataset.note = JSON.stringify(note);
+
+    noteListItems.push(li);
+  });
+
+  if (window.location.pathname === '/notes') {
+    noteListItems.forEach((note) => noteList[0].append(note));
+  }
+};
 
 
 
