a6dd5e66e05cc4fe55cd7655a6f8efafff7b93d6
Yutthana00 <arf101293@gmail.com>
2022-05-20 00:41:00 +0100

    " into '

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const fs = require("fs");
-const path = require("path");
-const express = require("express");
-const noteDatabase = require("./db/db.json");
-const uuid = require("uuid");
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const noteDatabase = require('./db/db.json');
+const uuid = require('uuid');
 
 const app = express();
 
@@ -13,27 +13,27 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // STATIC MIDDLEWARE
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 // Load index html landing page:
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
   });
 
 // Get notes page:
-app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/notes.html"));
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
 // GET /api/notes read the db.json file and return all saved notes as JSON.
-app.get("/api/notes", (req, res) => {
+app.get('/api/notes', (req, res) => {
   res.json(noteDatabase);
 });
 
 // POST /api/notes receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
-app.post("/api/notes", (req, res) => {
+app.post('/api/notes', (req, res) => {
   const newNote = {
-    id: uuid(),
+    id: uuid.v4(),
     title: req.body.title,
     text: req.body.text,
   };
@@ -42,21 +42,23 @@ app.post("/api/notes", (req, res) => {
   noteDatabase.push(newNote);
 
   // write to the db.json file to update the database
-  fs.writeFile("./db/db.json", JSON.stringify(noteDatabase), (err) => (err ? console.log(err) : console.log('success')));
+  fs.writeFile('./db/db.json', JSON.stringify(noteDatabase), (err) => (err ? console.log(err) : console.log('success')));
 
   // returns the note to the client
   res.json(noteDatabase);
 });
 
 // DELETE /api/notes/:id receives a query parameter that contains the id of a note to delete
-app.delete("/api/notes/:id", (req, res) => {
+app.delete('/api/notes/:id', (req, res) => {
   let noteId = req.params.id;
   let index = noteDatabase.findIndex((note) => note.id === noteId);
   let deletedNote = noteDatabase.splice(index, 1);
   res.send(deletedNote);
 
   // re-writes the file with updated database
-  fs.writeFile("./db/db.json", JSON.stringify(noteDatabase), (err) => (err ? console.log(err) : console.log('success')));
+  fs.writeFile('./db/db.json', JSON.stringify(noteDatabase, null, 2), (err) => {
+      if (err) throw err;
+});
 });
 
 app.listen(PORT, () => console.log(`App listening to ${PORT}`));
